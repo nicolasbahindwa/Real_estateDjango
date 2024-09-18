f9c1aa8d1b4240907da20fba7f465175802fe973
nicolasbahindwa <nicolasbahindwa@gmail.com>
2024-09-18 18:16:14 +0900

    add client react and redux

---
+++ client/src/app/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from '@reduxjs/toolkit';
+import propertyReducer from '../features/properties/propertySlice';
+
+
+export const store = configureStore({
+  reducer: {
+    properties: propertyReducer
+  },
+});
